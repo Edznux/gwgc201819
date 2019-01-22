13892b416d4b2799da48379270997653e84d9c6b
Edznux <edznux@gmail.com>
2019-01-22 20:50:06 +0100

    split into multiple canvas & fix terminal

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
-var w=1920
-var h=1080
+var w=1024
+var h=768
 
+var wTerm = 720;
+var hTerm = 480;
 
-TOPLEFT_SCREEN_1 = {x: 61, y:183}
-BOTTOMRIGHT_SCREEN_1 = {x: 1084, y:951}
-TOPLEFT_SCREEN_2 = {x: 1137, y:75}
-BOTTOMRIGHT_SCREEN_2 = {x: 1856, y:554}
+var cmds = [""]
+var cmdScroll = 0;
 
+TERM_SCROLL_OFFSET = 20;
 
 levelToColors = {
+    0: "FF0000",
     1: "FF5500",
     2: "00FF00",
     3: "55FF00",
@@ -16,39 +18,43 @@ levelToColors = {
     5: "FFFF55",
 }
 
-var E_SIZE = 200;
-var E_TOP    = { x: 183, y:500, type: 0}
-var E_LEFT   = { x:1000, y:300, type: 1}
-var E_RIGHT  = { x: 1000,y: 1700, type: 2 }
-var E_BOTTOM = { x: 951, y: 1700, type: 3 }
+var BLOCK_SIZE=64;
+
+var E_SIZE = 64;
+var E_TOP = { x: w / 2 - (E_SIZE/2), y: 0, type: 0}
+var E_LEFT = { x: 0, y: h / 2 - E_SIZE/2, type: 1}
+var E_RIGHT = { x: w - (E_SIZE/2),   y: h/2, type: 2 }
+var E_BOTTOM = { x: w / 2 - (E_SIZE / 2), y: h - (E_SIZE/2), type: 3 }
 
 var levels = [
     { 
         exits: [E_TOP, E_LEFT, E_RIGHT],
         blocks: [
-            {x:500,y:500},
-            {x:200,y:380},
-            {x:800,y:380},
-            {x:500,y:200}
+            { x: 0, y: BLOCK_SIZE*2}, // top left
+            { x: BLOCK_SIZE*2, y: h - BLOCK_SIZE}, // bottom left
+            { x: w - BLOCK_SIZE*3, y: 0}, // top right; *3 (*2 + itself)
+            { x: w - BLOCK_SIZE, y: h - BLOCK_SIZE*3} // bottom right
         ],
         lazers: [
-            { x: 532, y: 400, dir: "up", color: "red", active: true},
-            { x: 542, y: 400, dir: "down", color: "blue", active: true },
-            { x: 520, y: 400, dir: "right", color: "yellow", active: true },
-            { x: 510, y: 400, dir: "left", color: "white", active: true },
+            { x: w - (BLOCK_SIZE/2)*3 , y: h, dir: "up", color: "red", active: true},
+            { x: 0, y: h - (BLOCK_SIZE / 2) * 3, dir: "right", color: "yellow", active: true },
+            { x: (BLOCK_SIZE / 2) * 3, y: 0, dir: "down", color: "white", active: true },
+            { x: w, y: (BLOCK_SIZE / 2) * 3, dir: "left", color: "blue", active: true },
         ],
-        signals: [
-            { x: 1470, y: 150, level: 3, name: "energy" },
-            { x: 1470, y: 270, level: 4, name: "firewall" },
-            { x: 1700, y: 150, level: 5, name: "encryption" }
-        ]
+        signals: {
+            "energy": { x: 320, y: 80, level: 0},
+            "encryption": { x: 320, y: 200, level: 4 },
+            "firewall": { x: 565, y: 80, level: 5},
+        }
     }
 ]
 var current_level = 0;
 
 
 var c = document.getElementById("canvas");
+var cTerm = document.getElementById("terminal");
 var ctx = c.getContext("2d");
+var ctxTerm = cTerm.getContext("2d");
 var spritesheet = document.getElementById("spritesheet")
 
 
@@ -67,14 +73,13 @@ function drawDashboard(){
     drawSeparator()
     drawText()
     drawMiniMap()
-
 }
 
 
 function drawMiniMap(){
     var padding = 8
-    var offsetX = TOPLEFT_SCREEN_2.x + padding
-    var offsetY = TOPLEFT_SCREEN_2.y + padding
+    var offsetX = padding
+    var offsetY = padding
     for(var x=0; x < map.length; x++){
         for(var y=0; y < map[x].length; y++){
             sprite = map[x][y]
@@ -83,70 +88,91 @@ function drawMiniMap(){
                 srcy = (sprite * 32) - 32
                 posX = offsetX + (x * 32)
                 posY = offsetY + (y * 32)
-                ctx.drawImage(spritesheet, srcx, srcy, 32, 32, posX, posY, 32,32);
+                ctxTerm.drawImage(spritesheet, srcx, srcy, 32, 32, posX, posY, 32,32);
             }
         }
     }
 }
 
 function drawSeparator(){
-    ctx.beginPath();
-    var halfh = TOPLEFT_SCREEN_2.y + (BOTTOMRIGHT_SCREEN_2.y - TOPLEFT_SCREEN_2.y) / 2
-    var mapSize = halfh - TOPLEFT_SCREEN_2.y
+    ctxTerm.beginPath();
+    var halfh = (hTerm) / 2
     // split in half horizontally
-    ctx.strokeStyle = "#FEFEFE";
-    ctx.lineWidth = 2;
-    ctx.moveTo(TOPLEFT_SCREEN_2.x, halfh);
-    ctx.lineTo(BOTTOMRIGHT_SCREEN_2.x, halfh);
-    ctx.stroke();
+    ctxTerm.strokeStyle = "#FEFEFE";
+    ctxTerm.lineWidth = 2;
+    ctxTerm.moveTo(0, halfh);
+    ctxTerm.lineTo(wTerm, halfh);
+    ctxTerm.stroke();
     // map border
-    ctx.moveTo(TOPLEFT_SCREEN_2.x + mapSize, TOPLEFT_SCREEN_2.y);
-    ctx.lineTo(TOPLEFT_SCREEN_2.x + mapSize, TOPLEFT_SCREEN_2.y+mapSize);
-    ctx.stroke();
+    ctxTerm.moveTo(halfh, 0);
+    ctxTerm.lineTo(halfh, 0+halfh);
+    ctxTerm.stroke();
     
     // energy / firewall / encryption / ...
-    topRightPanelWidth = BOTTOMRIGHT_SCREEN_2.x - (TOPLEFT_SCREEN_2.x + mapSize)
-    topRightPanelHeight = BOTTOMRIGHT_SCREEN_2.y - halfh
+    topRightPanelWidth = wTerm - (halfh)
+    topRightPanelHeight = hTerm - halfh
     
-    ctx.moveTo(TOPLEFT_SCREEN_2.x + mapSize + (topRightPanelWidth/2), TOPLEFT_SCREEN_2.y);
-    ctx.lineTo(TOPLEFT_SCREEN_2.x + mapSize + (topRightPanelWidth/2), TOPLEFT_SCREEN_2.y+mapSize);
-    ctx.stroke();
+    ctxTerm.moveTo(halfh + (topRightPanelWidth/2), 0);
+    ctxTerm.lineTo(halfh + (topRightPanelWidth/2), halfh);
+    ctxTerm.stroke();
     
-    ctx.moveTo(TOPLEFT_SCREEN_2.x + mapSize, TOPLEFT_SCREEN_2.y + (mapSize/2));
-    ctx.lineTo(BOTTOMRIGHT_SCREEN_2.x, TOPLEFT_SCREEN_2.y + (mapSize / 2));
-    ctx.stroke();
-    ctx.closePath();
+    ctxTerm.moveTo(halfh, (halfh/2));
+    ctxTerm.lineTo(wTerm, (halfh / 2));
+    ctxTerm.stroke();
+    ctxTerm.closePath();
 }
 
 function drawText(){
-    ctx.font = "30px Arial";
-    ctx.strokeStyle = "#00FF00";
-    ctx.strokeText("Energy", 1440, 110); 
-    ctx.strokeText("Encryption", 1415, 230); 
-    ctx.strokeText("Firewall", 1670, 110); 
+    ctxTerm.beginPath();
+    ctxTerm.font = "30px Arial";
+    ctxTerm.strokeStyle = "#00FF00";
+    ctxTerm.strokeText("Energy", 310, 40); 
+    ctxTerm.strokeText("Encryption", 290, 160); 
+    ctxTerm.strokeText("Firewall", 545, 40);
+    ctxTerm.closePath();
 }
 
 function drawSignal(x, y, n, c){
-    ctx.beginPath();
+    ctxTerm.beginPath();
     var spacing = 8
-    ctx.strokeStyle = c
-    for (var line = 1; line <= n; line++){
-        ctx.moveTo(x + (line * spacing), y - (5 * line))
-        ctx.lineTo(x + (line * spacing), y + (5 * line) )
-        ctx.stroke();
+    var nline = n
+    ctxTerm.strokeStyle = "#"+c
+    if(n>5){
+        nline = 5
     }
-    ctx.closePath();
+    if(n<0){
+        nline = 0
+    }
+    if (nline > 0){
+        for (var line = 1; line <= nline; line++){
+            ctxTerm.moveTo(x + (line * spacing), y - (5 * line))
+            ctxTerm.lineTo(x + (line * spacing), y + (5 * line) )
+            ctxTerm.stroke();
+        }
+    }else{
+        ctxTerm.moveTo(x,    y-10)
+        ctxTerm.lineTo(x+20, y+10)
+        ctxTerm.stroke();
+
+        ctxTerm.moveTo(x+20, y-10)
+        ctxTerm.lineTo(x,    y+10)
+        ctxTerm.stroke();
+    }
+
+    ctxTerm.font = "30px Arial";
+    ctxTerm.strokeText(n, x+50, y+10); 
+    ctxTerm.closePath();
 }
+
 function drawSignals(signals){
-    console.log(signals)
-    for(var i = 0; i < signals.length; i++){
-        var s = signals[i]
-        drawSignal(s.x, s.y, s.level, levelToColors[s.level])
+    for (var k in signals) {
+        if (signals.hasOwnProperty(k)) {
+            drawSignal(signals[k].x, signals[k].y, signals[k].level, levelToColors[signals[k].level])
+        }
     }
 }
 
 function drawExits(exits){
-    console.log(exits)
     for(var i = 0; i < exits.length; i++){
         var exit=exits[i] 
         ctx.drawImage(spritesheet, 32 + (exit.type*64), 128, 64, 64, exit.x, exit.y, 64, 64)
@@ -167,14 +193,14 @@ function drawLazer(lazers, blocks){
         switch (lazer.dir) {
             case "up":
                 endX = lazer.x
-                endY = TOPLEFT_SCREEN_1.y
+                endY = 0
                 for(var b = 0; b < blocks.length; b++){
                     block = blocks[b]
-                    if(lazer.x > block.x && lazer.x < block.x + 64 && lazer.y > block.y){
+                    if(lazer.x > block.x && lazer.x < block.x + BLOCK_SIZE && lazer.y > block.y){
                         console.log("Block obstructing lazer")
                         endX = lazer.x
                         if (endY < block.y) {
-                            endY = block.y + 64
+                            endY = block.y + BLOCK_SIZE
                         }
                     }
                 }
@@ -183,10 +209,10 @@ function drawLazer(lazers, blocks){
                 break;
             case "down":
                 endX = lazer.x
-                endY = BOTTOMRIGHT_SCREEN_1.y
+                endY = h
                 for(var b = 0; b < blocks.length; b++){
                     block = blocks[b]
-                    if (lazer.x > block.x && lazer.x < block.x + 64 && lazer.y < block.y){
+                    if (lazer.x > block.x && lazer.x < block.x + BLOCK_SIZE && lazer.y < block.y){
                         console.log("Block obstructing lazer")
                         endX = lazer.x
                         if (endY > block.y) {
@@ -198,11 +224,11 @@ function drawLazer(lazers, blocks){
                 ctx.stroke();
                 break
             case "right":
-                endX = BOTTOMRIGHT_SCREEN_1.x
+                endX = w
                 endY = lazer.y
                 for(var b = 0; b < blocks.length; b++){
                     block = blocks[b]
-                    if(lazer.y > block.y && lazer.y < block.y + 64 && lazer.x < block.x){
+                    if(lazer.y > block.y && lazer.y < block.y + BLOCK_SIZE && lazer.x < block.x){
                         if (endX > block.x){
                             endX = block.x
                         }
@@ -214,13 +240,13 @@ function drawLazer(lazers, blocks){
                 break
                 
             case "left":
-                endX = TOPLEFT_SCREEN_1.x
+                endX = 0
                 endY = lazer.y
                 for(var b = 0; b < blocks.length; b++){
                     block = blocks[b]
-                    if (lazer.y > block.y && lazer.y < block.y + 64 && lazer.x > block.x){
+                    if (lazer.y > block.y && lazer.y < block.y + BLOCK_SIZE && lazer.x > block.x){
                         if (endX < block.x) {
-                            endX = block.x+64
+                            endX = block.x+BLOCK_SIZE
                         }
                         endY = lazer.y
                     }
@@ -244,6 +270,8 @@ function drawLazer(lazers, blocks){
 // - start (starting position)
 // - signals
 function drawLevel(level){
+    ctx.fillRect(0,0, w, h)
+    // ctxTerm.fillRect(0, hTerm / 2, wTerm, hTerm / 2)
     // console.log(level.blocks)
     drawBlock(level.blocks)
     drawLazer(level.lazers, level.blocks)
@@ -262,16 +290,88 @@ function drawClock(){
 
 }
 
+// redraw this inside a new canvas so we can scroll (like a paralax) without clearing the whole screen
+function drawTerminal(){
+    ctxTerm.fillRect(0, hTerm/2,wTerm, hTerm/2)
+    var padding = 5;
+    ctxTerm.font = "15px Arial";
+    ctxTerm.strokeStyle = "#00FF00";
+
+    for(var i = 0; i < cmds.length; i++ ){
+        ctxTerm.strokeText("Hunter2> " + cmds[i], 10, hTerm / 2 + ((i+1) * TERM_SCROLL_OFFSET) + padding + cmdScroll);
+    }
+}
+
+function removeColor(color){
+    console.log(levels[current_level])
+    for (var i = 0; i < levels[current_level].lazers.length; i++){
+        if (levels[current_level].lazers[i].color == color){
+            if (levels[current_level].signals["energy"].level > 0){
+                levels[current_level].signals["energy"].level--
+                levels[current_level].lazers[i].active = false
+                console.log("disabled ", levels[current_level].lazers[i])
+            }else{
+                console.log("not enough energy")
+            }
+        }
+    }
+}
+
+function executeCmd(cmd){
+    var splitted = cmd.split(" ")
+    if(splitted[0] == "remove"){
+        if(splitted[1] == "yellow" || splitted[1] == "red" || splitted[1] == "blue" || splitted[1] == "white"){
+            console.log("call to removeColor")
+            removeColor(splitted[1])
+            drawLevel(levels[current_level])
+        }else{
+            console.log("invalid color")
+        }
+    }
+    // console.log(splitted)
+}
+
+function onKeydown(e) {
+    if (e.keyCode == 8) { // backspace
+        if (cmds[cmds.length - 1].length > 0){
+            cmds[cmds.length - 1] = cmds[cmds.length - 1].slice(0, -1);
+        }
+    }
+
+    if (e.keyCode == 13) { // enter
+        if (cmds.length > 10){
+            cmds.shift()
+        }
+        executeCmd(cmds[cmds.length - 1])
+        cmds.push("")
+        drawTerminal()
+        return
+    }
+
+    char = String.fromCharCode(e.keyCode).toLowerCase()
+    if (char.match(/[a-z ]/g)){
+        cmds[cmds.length - 1] += char
+    }
+    drawTerminal()
+}
 
 function main(){
-    ctx.fillStyle = "#0E0E0E";
-    ctx.fillRect(0, 0, w, h);
-
-    drawDashboard()
-    drawLevel(levels[current_level])
-    // drawSignals(SIGNALS_ENERGY, SIGNALS_FIREWALL, SIGNALS_ENCRYPTION)
+    
+    setInterval(() => {
+        ctx.fillStyle = "#0E0E0E";
+        ctx.fillRect(0, 0, w, h);
+    
+        ctxTerm.fillStyle = "#0A0A0A";
+        ctxTerm.fillRect(0, 0, w, h);
+        drawDashboard()
+        drawTerminal()
+        drawLevel(levels[current_level])
+    }, 16);
+    
 }
+
 
 window.onload = function () {
     main()
+    window.addEventListener('keydown', onKeydown, false);
 }
